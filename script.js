9f4a37835a0eadb20685f91cd9989b57f43d61ce
OlgVol <deviatkovaolga@gmail.com>
2022-07-15 13:38:50 +0300

    Js main project

---
+++ script.js
@@ -0,0 +1,32 @@
+function computerPlay() {
+let option = ['rock', 'paper','scissors'];
+return option[Math.floor(Math.random() * option.length)];
+}
+const winner ="You are the winner!"
+
+function playRound(playerSelection, computerSelection) {
+if (playerSelection === computerSelection) {
+    return `We both are winers ${playerSelection}`;
+} else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return `${winner} ${playerSelection} beats ${computerSelection}`;
+} else if (playerSelection === "paper" && computerSelection === "rock") {
+    return `${winner} ${playerSelection} beats ${computerSelection}`;
+} else if (playerSelection === "scissors" && computerSelection
+=== "paper") {
+    return `${winner} ${playerSelection} beats ${computerSelection}`;
+}else {
+    return `You lose! ${computerSelection} beats ${playerSelection}`
+}
+}
+const playerSelection = prompt("Start the game. Please write the option: ' Rock, Paper or Scissors'").toLowerCase(); 
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
+
+function game() {
+    for (let i = 1; i < 5; i++) {
+        let playerSelection = prompt('Choose your weapon');
+        const computerSelection = computerPlay();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+}
+console.log(game());
