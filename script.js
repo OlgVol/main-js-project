c96999ff76d5fbb3d8f6c69082c8b80fc655e70f
OlgVol <deviatkovaolga@gmail.com>
2022-07-20 12:08:59 +0300

    added error option

--- script.js
+++ script.js
@@ -4,31 +4,51 @@ return option[Math.floor(Math.random() * option.length)];
 }
 const winner ="You are the winner!"
 
+function playerPlay() {
+    let option = ['rock', 'paper', 'scissors']
+}
+
 function playRound(playerSelection, computerSelection) {
 if (playerSelection === computerSelection) {
-    return `We both are winers ${playerSelection}`;
+    return {
+        message : `We both are winers ${playerSelection}`,
+        status: "winner" }
 } else if (playerSelection === "rock" && computerSelection === "scissors") {
-    return `${winner} ${playerSelection} beats ${computerSelection}`;
+    return {
+        message : `${winner} ${playerSelection} beats ${computerSelection}`,
+        status : "winner" }
 } else if (playerSelection === "paper" && computerSelection === "rock") {
-    return `${winner} ${playerSelection} beats ${computerSelection}`;
+    return {
+        message: `${winner} ${playerSelection} beats ${computerSelection}`,
+        status: "winner" }
 } else if (playerSelection === "scissors" && computerSelection
 === "paper") {
-    return `${winner} ${playerSelection} beats ${computerSelection}`;
-}else {
-    return `You lose! ${computerSelection} beats ${playerSelection}`
+    return {
+        message : `${winner} ${playerSelection} beats ${computerSelection}`,
+        status: "winner" }
+} else if (playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors") {
+    return {
+        message : `Please write valid option`,
+        status : "error" }
+} else {
+  return {
+    message :`You lose! ${computerSelection} beats ${playerSelection}`,
+    status : "lose" }
 }
-}
+}   
+
 const playerSelection = prompt("Start the game. Please write the option: ' Rock, Paper or Scissors'").toLowerCase(); 
 const computerSelection = computerPlay();
 console.log(playRound(playerSelection, computerSelection));
-alert(playRound(playerSelection, computerSelection));
 
 function game() {
     for (let i = 1; i < 5; i++) {
         let playerSelection = prompt("Choose your next option from 'Rock, Paper or Scissors' ");
         const computerSelection = computerPlay();
         console.log(playRound(playerSelection, computerSelection));
-        alert(playRound(playerSelection, computerSelection));
+        
     }
 }
 console.log(game());
+
+
